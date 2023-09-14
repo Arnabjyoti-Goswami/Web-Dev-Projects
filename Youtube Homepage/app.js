470b410aaa5526f37f5bb39d6c299d297fbdded5
PyDawg <119682819+Arnabjyoti-Goswami@users.noreply.github.com>
2023-09-14 09:52:26 +0530

    Add files via upload

---
+++ Youtube Homepage/app.js
@@ -0,0 +1,162 @@
+document.querySelector('.youtube-logo').addEventListener('click', () => {
+  const url = 'https://www.youtube.com'
+  window.open(url, "_blank");
+});
+
+
+// Open the actual YouTube videos and the Youtube channels upon clicking the relevant elements of the webpage:
+document.querySelectorAll('.open-video-1-js').forEach((element) => {
+  element.addEventListener('click', () => {
+  const url = 'https://www.youtube.com/watch?v=n2RNcPRtAiY&ab_channel=MarquesBrownlee';
+  window.open(url, '_blank'); // window.location.href = url;  to open the new url in the same tag
+  });
+});
+document.querySelectorAll('.open-channel-1-js').forEach((element) => {
+  element.addEventListener('click', () => {
+  const url = 'https://www.youtube.com/@mkbhd';
+  window.open(url, '_blank');
+  });
+});
+
+document.querySelectorAll('.open-video-2-js').forEach((element) => {
+  element.addEventListener('click', () => {
+  const url = 'https://www.youtube.com/watch?v=mP0RAo9SKZk&ab_channel=Markiplier';
+  window.open(url, '_blank');
+  });
+});
+document.querySelectorAll('.open-channel-2-js').forEach((element) => {
+  element.addEventListener('click', () => {
+  const url = 'https://www.youtube.com/@markiplier';
+  window.open(url, '_blank');
+  });
+});
+
+document.querySelectorAll('.open-video-3-js').forEach((element) => {
+  element.addEventListener('click', () => {
+  const url = 'https://www.youtube.com/watch?v=FgjPQQeTh1w&ab_channel=SSSniperWolf';
+  window.open(url, '_blank');
+  });
+});
+document.querySelectorAll('.open-channel-3-js').forEach((element) => {
+  element.addEventListener('click', () => {
+  const url = 'https://www.youtube.com/@SSSniperWolf';
+  window.open(url, '_blank');
+  });
+});
+
+document.querySelectorAll('.open-video-4-js').forEach((element) => {
+  element.addEventListener('click', () => {
+  const url = 'https://www.youtube.com/watch?v=094y1Z2wpJg&ab_channel=Veritasium';
+  window.open(url, '_blank');
+  });
+});
+document.querySelectorAll('.open-channel-4-js').forEach((element) => {
+  element.addEventListener('click', () => {
+  const url = 'https://www.youtube.com/@veritasium';
+  window.open(url, '_blank');
+  });
+});
+
+document.querySelectorAll('.open-video-5-js').forEach((element) => {
+  element.addEventListener('click', () => {
+  const url = 'https://www.youtube.com/watch?v=86CQq3pKSUw&ab_channel=CSDojo';
+  window.open(url, '_blank');
+  });
+});
+document.querySelectorAll('.open-channel-5-js').forEach((element) => {
+  element.addEventListener('click', () => {
+  const url = 'https://www.youtube.com/@CSDojo';
+  window.open(url, '_blank');
+  });
+});
+
+document.querySelectorAll('.open-video-6-js').forEach((element) => {
+  element.addEventListener('click', () => {
+  const url = 'https://www.youtube.com/watch?v=yXWw0_UfSFg';
+  window.open(url, '_blank');
+  });
+});
+document.querySelectorAll('.open-channel-6-js').forEach((element) => {
+  element.addEventListener('click', () => {
+  const url = 'https://www.youtube.com/@MrBeast';
+  window.open(url, '_blank');
+  });
+});
+
+document.querySelectorAll('.open-video-7-js').forEach((element) => {
+  element.addEventListener('click', () => {
+  const url = 'https://www.youtube.com/watch?v=fNVa1qMbF9Y&ab_channel=RealLifeLore';
+  window.open(url, '_blank');
+  });
+});
+document.querySelectorAll('.open-channel-7-js').forEach((element) => {
+  element.addEventListener('click', () => {
+  const url = 'https://www.youtube.com/@RealLifeLore';
+  window.open(url, '_blank');
+  });
+});
+
+document.querySelectorAll('.open-video-8-js').forEach((element) => {
+  element.addEventListener('click', () => {
+  const url = 'https://www.youtube.com/watch?v=lFm4EM1juls&ab_channel=TechVision';
+  window.open(url, '_blank');
+  });
+});
+document.querySelectorAll('.open-channel-8-js').forEach((element) => {
+  element.addEventListener('click', () => {
+  const url = 'https://www.youtube.com/@adin';
+  window.open(url, '_blank');
+  });
+});
+
+document.querySelectorAll('.open-video-9-js').forEach((element) => {
+  element.addEventListener('click', () => {
+  const url = 'https://www.youtube.com/watch?v=ixmxOlcrlUc&ab_channel=THENX';
+  window.open(url, '_blank');
+  });
+});
+document.querySelectorAll('.open-channel-9-js').forEach((element) => {
+  element.addEventListener('click', () => {
+  const url = 'https://www.youtube.com/@OFFICIALTHENXSTUDIOS';
+  window.open(url, '_blank');
+  });
+});
+
+document.querySelectorAll('.open-video-10-js').forEach((element) => {
+  element.addEventListener('click', () => {
+  const url = 'https://www.youtube.com/watch?v=R2vXbFp5C9o&ab_channel=InsiderBusiness';
+  window.open(url, '_blank');
+  });
+});
+document.querySelectorAll('.open-channel-10-js').forEach((element) => {
+  element.addEventListener('click', () => {
+  const url = 'https://www.youtube.com/@InsiderBusiness';
+  window.open(url, '_blank');
+  });
+});
+
+document.querySelectorAll('.open-video-11-js').forEach((element) => {
+  element.addEventListener('click', () => {
+  const url = 'https://www.youtube.com/watch?v=0nZuYyXET3s&ab_channel=DestinationTips';
+  window.open(url, '_blank');
+  });
+});
+document.querySelectorAll('.open-channel-11-js').forEach((element) => {
+  element.addEventListener('click', () => {
+  const url = 'https://www.youtube.com/@Destinationtips';
+  window.open(url, '_blank');
+  });
+});
+
+document.querySelectorAll('.open-video-12-js').forEach((element) => {
+  element.addEventListener('click', () => {
+  const url = 'https://www.youtube.com/watch?v=9iMGFqMmUFs&ab_channel=TED-Ed';
+  window.open(url, '_blank');
+  });
+});
+document.querySelectorAll('.open-channel-12-js').forEach((element) => {
+  element.addEventListener('click', () => {
+  const url = 'https://www.youtube.com/@TEDEd';
+  window.open(url, '_blank');
+  });
+});
