9ace8f483de96f2328d029a0935ab270f762fa39
PyDawg <119682819+Arnabjyoti-Goswami@users.noreply.github.com>
2023-09-18 22:30:19 +0530

    functional cart summary including shipping cost

--- Amazon shopping and cart pages/scripts/checkout.js
+++ Amazon shopping and cart pages/scripts/checkout.js
@@ -116,6 +116,7 @@ document.querySelectorAll('.delete-product-js').forEach( (link) => {
     document.querySelector(`.cart-item-container-${currentProductId}-js`).remove(); //remove the html element for the deleted product from the webpage
 
     updateCartQuantityHeader();
+    updatePaymentSummary();
   });
 });
 
@@ -150,21 +151,104 @@ function updateQuantity(productId, quantity) {
       cartItem.quantity = quantity;
     }
   } );
+  updatePaymentSummary();
   saveToStorage();
 }
 
 
 // Make the delivery date options functional
-document.querySelectorAll('.delivery-option-input').forEach((radio) => {
+document.querySelectorAll('.delivery-option-input').forEach( (radio) => {
   radio.addEventListener('change', () => {
-    const currentProductId = radio.getAttribute('name').replace('delivery-option-', ''); // get the productId for which the radio button selector on which a change occurs
-    const deliveryDateElement = document.querySelector(`.delivery-date-${currentProductId}-js`); // select the delivery date element so that we can change its innerHTML according to the option selected
+    const currentProductId = radio.getAttribute('name').replace('delivery-option-', ''); // get the productId for the radio button selector on which a change occurs
+    const deliveryDateElement = document.querySelector(`.delivery-date-${currentProductId}-js`)
     const selectedOption = document.querySelector(`input[name="delivery-option-${currentProductId}"]:checked`); // get the selected option out of the all the radio selector options available for the current product
     
-    // Get the selected delivery date and update the delivery date element
+    // If an option is selected, get the selected delivery date, and update the delivery date element
     if (selectedOption) {
       const selectedDeliveryDate = selectedOption.nextElementSibling.querySelector('.delivery-option-date').textContent; // get the delivery date data for the selectedOption
-      deliveryDateElement.textContent = `Delivery date: ${selectedDeliveryDate}`; // update the innerHTML of the delivery date element
+
+      deliveryDateElement.textContent = `Delivery date: ${selectedDeliveryDate}`; // update the innerHTML of the delivery date element with the delivery data data
     }
+    
+    updatePaymentSummary();
   });
 });
+
+
+
+// Generate the html for the payment summary and make it functional
+document.querySelector('.payment-summary-js').innerHTML = `
+<div class="payment-summary-title">
+  Order Summary
+</div>
+
+<div class="payment-summary-row">
+  <div>Items (3):</div>
+  <div class="payment-summary-money  items-total-cost-js"></div>
+</div>
+
+<div class="payment-summary-row">
+  <div>Shipping &amp; handling:</div>
+  <div class="payment-summary-money  shipping-total-cost-js"></div>
+</div>
+
+<div class="payment-summary-row subtotal-row">
+  <div>Total before tax:</div>
+  <div class="payment-summary-money  total-before-tax-js"></div>
+</div>
+
+<div class="payment-summary-row">
+  <div>Estimated tax (10%):</div>
+  <div class="payment-summary-money  tax-js"></div>
+</div>
+
+<div class="payment-summary-row total-row">
+  <div>Order total:</div>
+  <div class="payment-summary-money  order-total-cost-js"></div>
+</div>
+
+<button class="place-order-button button-primary">
+  Place your order
+</button>
+`
+updatePaymentSummary(); // intial function call to set the correct payment summary
+
+function calculateItemsTotalCost() {
+  let itemsTotalCost = 0;
+  cart.forEach( (cartItem) => {
+    const matchingProduct = products.find((product) => product.id === cartItem.productId);
+    itemsTotalCost += matchingProduct.priceCents * cartItem.quantity;
+  });
+  return itemsTotalCost;
+}
+function calculateShippingTotalCost() {
+  let shippingTotalCost = 0;
+  // Iterate over each selected delivery option:
+  document.querySelectorAll('.delivery-option-input:checked').forEach( (radio) => {
+    let selectedOptionShippingPriceData = radio.nextElementSibling.querySelector('.delivery-option-price').textContent; // get the shipping price text for the selected option
+    selectedOptionShippingPriceData = selectedOptionShippingPriceData.replace(/[^0-9]/g, ''); // remove all the characters from the text except the digits 0-9
+    const selectedOptionShippingPriceCents = Number(selectedOptionShippingPriceData);
+    shippingTotalCost += selectedOptionShippingPriceCents; 
+  });
+  return shippingTotalCost;
+}
+function calculateTax(itemsTotalCost, shippingTotalCost) {
+  const taxRate = 0.10;
+  return (itemsTotalCost + shippingTotalCost) * taxRate;
+}
+
+// Function to update the payment summary
+function updatePaymentSummary() {
+  const itemsTotalCost = calculateItemsTotalCost();
+  const shippingTotalCost = calculateShippingTotalCost();
+  const totalBeforeTax = itemsTotalCost + shippingTotalCost;
+  const tax = calculateTax(itemsTotalCost, shippingTotalCost);
+  const orderTotal = itemsTotalCost + shippingTotalCost + tax;
+
+  // Update the HTML elements with the calculated values:
+  document.querySelector('.items-total-cost-js').textContent = `$${formatCurrency(itemsTotalCost)}`;
+  document.querySelector('.shipping-total-cost-js').textContent = `$${formatCurrency(shippingTotalCost)}`;
+  document.querySelector('.total-before-tax-js').textContent = `$${formatCurrency(totalBeforeTax)}`;
+  document.querySelector('.tax-js').textContent = `$${formatCurrency(tax)}`;
+  document.querySelector('.order-total-cost-js').textContent = `$${formatCurrency(orderTotal)}`;
+}
